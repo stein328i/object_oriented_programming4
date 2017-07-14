a76dd3373e11bccb28fc1b857f1338e3ac559c8e
stein328i <stein.328i@gmail.com>
2017-07-14 16:02:32 -0400

    updates

--- web_crm_part4/crm.rb
+++ web_crm_part4/crm.rb
@@ -1,5 +1,4 @@
 require_relative 'contact'
-
 require 'sinatra'
 
 
@@ -46,6 +45,41 @@ post '/contacts' do
     note:       params[:note]
   )
   redirect to('/contacts')
+end
+
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 
